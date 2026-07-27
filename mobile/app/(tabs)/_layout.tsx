3577b9803f563ed8bef4f180997cbd0b324ec588
ishevchyk <irynashevchykk@gmail.com>
2026-07-27 19:21:06 +0300

    Add tab bar icons and fix post-auth redirect

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import {Button} from 'react-native';
 import {Tabs, useRouter} from 'expo-router';
+import {MaterialDesignIcons} from '@react-native-vector-icons/material-design-icons';
 
 import {useThemeColor} from '../../hooks/useThemeColor';
 
@@ -9,11 +10,14 @@ export default function TabsLayout() {
   return (
       <Tabs screenOptions={{ tabBarActiveTintColor: colors.primary, tabBarInactiveTintColor: colors.textSecondary }}>
           <Tabs.Screen name="products"  options={{
-              title: 'Products' ,
-              headerRight: () => <Button title="+" color={colors.primary} onPress={() => router.push('/add-product')} />
+              title: 'Products',
+              headerRight: () => <Button title="+ ADD" color={colors.primary} onPress={() => router.push('/add-product')} />,
+              tabBarIcon: ({focused}) => <MaterialDesignIcons name={"barley"} size={20} color={focused ? colors.primary : colors.surface} />,
           }} />
           <Tabs.Screen name="recipes" options={{
               title: 'Recipes',
+              tabBarIcon: ({focused}) => <MaterialDesignIcons name={"bowl-mix"} size={20} color={focused ? colors.primary : colors.surface} />,
+              tabBarActiveTintColor: colors.primary
           }} />
       </Tabs>
   );
